a77b90316c21ffaebf5eee8c9cbd3afbfbb526b4
Ernesto Krsulovic <ernesto.krsulovic@gmail.com>
2023-04-19 05:09:04 +0000

    Update 27 files

--- pages/examples.tsx
+++ pages/examples.tsx
@@ -1,21 +1,24 @@
 import Head from "next/head"
+import Image from "next/image"
 
 import { Layout } from "@/components/layout"
 
 export default function IndexPage() {
   var samples = [
-"26e6ddce-be5b-4c1c-ac2a-e1dadd82d45c_182673.jpg",
-"27f66896-2055-47ad-9e9e-f281acd7d00c_776729.jpg",
-"61e6ff9c-7242-4945-8f98-2d888618bb4d_413618.jpg",
-"76b2cbc0-0968-4ce7-9e33-221a6466c888_608576.jpg",
-"7814b644-be44-4886-80e4-32c209ee6f90_456484.jpg",
-"9330f128-a588-46e8-83d8-72a0593e7baa_768431.jpg",
-"989a5f62-2e96-4871-acc6-71c600a69e6b_651523.jpg",
-"ad22405d-76ab-4c80-be45-6444822823ee_482935.jpg",
-"af6dcb00-78f9-460f-821e-1e4578ff244b_255404.jpg",
-"d8adeb2c-e2a2-449c-8971-2e79a1a30563_213372.jpg",
-"df32c5e6-45b6-48b8-a991-896d308a4f06_896809.jpg",
-"ef438419-a8c3-4f91-a64a-a53fe8c53991_120516.jpg",]
+"26e6ddce-be5b-4c1c-ac2a-e1dadd82d45c_182673-transparent.png",
+"27f66896-2055-47ad-9e9e-f281acd7d00c_776729-transparent.png",
+"61e6ff9c-7242-4945-8f98-2d888618bb4d_413618-transparent.png",
+"76b2cbc0-0968-4ce7-9e33-221a6466c888_608576-transparent.png",
+"7814b644-be44-4886-80e4-32c209ee6f90_456484-transparent.png",
+"9330f128-a588-46e8-83d8-72a0593e7baa_768431-transparent.png",
+"989a5f62-2e96-4871-acc6-71c600a69e6b_651523-transparent.png",
+"ad22405d-76ab-4c80-be45-6444822823ee_482935-transparent.png",
+"af6dcb00-78f9-460f-821e-1e4578ff244b_255404-transparent.png",
+"d8adeb2c-e2a2-449c-8971-2e79a1a30563_213372-transparent.png",
+"df32c5e6-45b6-48b8-a991-896d308a4f06_896809-transparent.png",
+"ef438419-a8c3-4f91-a64a-a53fe8c53991_120516-transparent.png",
+"fdf1ca25-3627-4d5f-885f-578fc49a0363_223423-transparent.png",
+]
   
   
   return (
@@ -28,8 +31,7 @@ export default function IndexPage() {
       </Head>
       <section className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center gap-6 pt-6 pb-8 md:py-10">
         {samples.map((img, index) => {
-              let file = "/examples/" + img
-              return (<img src={file} className="border-4 border-white rounded-2xl" />);
+              return (<Image alt={"example-"+index} src={"/examples/" + img} width="512" height="512"/>);
         })}
       
       </section>
